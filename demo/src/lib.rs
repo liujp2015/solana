188c63c72fde921cb5f3e1284660f9872b7d660b
liujp2015 <392303104@qq.com>
2026-01-12 15:10:21 +0800

    feet:first rust

---
+++ demo/src/lib.rs
@@ -0,0 +1,14 @@
+use solana_program::{account_info::AccountInfo, entrypoint,entrypoint::ProgramResult, msg, pubkey::Pubkey};
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: Pubkey,
+    accounts: &[AccountInfo],
+    data: &[u8],
+) -> ProgramResult {
+    msg!("Hello Solana!");
+    Ok(())
+}
+
+
