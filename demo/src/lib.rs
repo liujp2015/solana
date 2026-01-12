2519eba2a0caa088a819f5f0522cf68ddcd2b219
liujp2015 <392303104@qq.com>
2026-01-12 17:35:36 +0800

    commit

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -1,27 +1,27 @@
 #![allow(unexpected_cfgs)]
 
-use solana_program::sysvar::Sysvar;
+use solana_program::{account_info::{AccountInfo, next_account_info},entrypoint, entrypoint:: ProgramResult, program::invoke_signed, pubkey::Pubkey, rent::Rent, system_instruction::create_account, sysvar::Sysvar};
 
-solana_program::entrypoint!(process_instruction);
+entrypoint!(process_instruction);
 
 pub fn process_instruction(
-    program_id: &solana_program::pubkey::Pubkey,
-    accounts: &[solana_program::account_info::AccountInfo],
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
     data: &[u8],
-) -> solana_program::entrypoint::ProgramResult {
+) -> ProgramResult {
     let accounts_iter = &mut accounts.iter();
-    let account_user = solana_program::account_info::next_account_info(accounts_iter)?;
-    let account_data = solana_program::account_info::next_account_info(accounts_iter)?;
-    let _ = solana_program::account_info::next_account_info(accounts_iter)?; // Program system
-    let _ = solana_program::account_info::next_account_info(accounts_iter)?; // Program sysvar rent
+    let account_user = next_account_info(accounts_iter)?;
+    let account_data = next_account_info(accounts_iter)?;
+    let _ = next_account_info(accounts_iter)?; // Program system
+    let _ = next_account_info(accounts_iter)?; // Program sysvar rent
 
-    let rent_exemption = solana_program::rent::Rent::get()?.minimum_balance(data.len());
-    let bump_seed = solana_program::pubkey::Pubkey::find_program_address(&[&account_user.key.to_bytes()], program_id).1;
+    let rent_exemption = Rent::get()?.minimum_balance(data.len());
+    let bump_seed = Pubkey::find_program_address(&[&account_user.key.to_bytes()], program_id).1;
 
     // Data account is not initialized. Create an account and write data into it.
     if **account_data.try_borrow_lamports().unwrap() == 0 {
-        solana_program::program::invoke_signed(
-            &solana_program::system_instruction::create_account(
+       invoke_signed(
+            &create_account(
                 account_user.key,
                 account_data.key,
                 rent_exemption,
