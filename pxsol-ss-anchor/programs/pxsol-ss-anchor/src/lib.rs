c7056d36eee6dbda7c0ad7bf5fd8b84b810e64b7
liujp2015 <392303104@qq.com>
2026-01-18 18:53:31 +0800

    feat: commit

--- pxsol-ss-anchor/programs/pxsol-ss-anchor/src/lib.rs
+++ pxsol-ss-anchor/programs/pxsol-ss-anchor/src/lib.rs
@@ -5,12 +5,31 @@ declare_id!("2KzMeNKbe8pKqT9aMhQpQuNHH9JkMFwNNANxQHvUoCko");
 #[program]
 pub mod pxsol_ss_anchor {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn init(ctx: Context<Init>) -> Result<()> {
+        let account_user = &ctx.accounts.user;
+        let account_user_pda = &mut ctx.accounts.user_pda;
+        account_user_pda.auth = account_user.key();
+        account_user_pda.bump = ctx.bumps.user_pda;
+        account_user_pda.data = Vec::new();
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Init {}
+
+
+
+#[account]
+pub struct Data {
+    pub auth: Pubkey, // The owner of this pda account
+    pub bump: u8,     // The bump to generate the PDA
+    pub data: Vec<u8> // The content, arbitrary bytes
+}
+
+impl Data {
+    pub fn space_for(data_len: usize) -> usize {
+        // 8 (discriminator) + 32 (auth) + 1 (bump) + 4 (vec len) + data_len
+        8 + 32 + 1 + 4 + data_len
+    }
+}
