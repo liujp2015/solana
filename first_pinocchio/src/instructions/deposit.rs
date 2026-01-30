1514d8571528babb8d0b400c87bded5968b5862a
liujp2015 <392303104@qq.com>
2026-01-30 15:24:56 +0800

    feat:finishing deposit feature of first pinocchio project

--- first_pinocchio/src/instructions/deposit.rs
+++ first_pinocchio/src/instructions/deposit.rs
@@ -1,23 +1,53 @@
-use pinocchio::{AccountView, ProgramResult, error::ProgramError};
+use pinocchio::{AccountView, ProgramResult, error::ProgramError,Address};
+use pinocchio_system::instructions::Transfer;
+use solana_program::{log, program_error};
 use solana_program_log::log;
+
 
 pub struct Deposit<'info>{
     owner:&'info AccountView,
     vault:&'info AccountView,
-    amount:u64,
+    lamports:u64,
 }
+
+impl<'info> TryFrom<(&'info [AccountView], &'info [u8])> for Deposit<'info>{
+   type Error = ProgramError;
+   fn try_from(value: (&'info [AccountView], &'info [u8])) -> Result<Self, Self::Error> {
+     let [owner,vault,_] = value.0 else {
+         return Err(ProgramError::NotEnoughAccountKeys);
+     };
+     // Checks owner is a signer
+     if !owner.is_signer(){
+        return Err(ProgramError::MissingRequiredSignature);
+     }
+     // check valult belongs to System Program
+     if !vault.owned_by(&pinocchio_system::ID){
+        return Err(ProgramError::InvalidAccountOwner);
+     }
+     // check vault address matchs
+     let vault_address: Address = Address::find_program_address(&[b"vault'",owner.address().as_ref()],&crate::ID).0;
+     if vault_address.ne(vault.address()) {
+        return Err(ProgramError::InvalidAccountData);
+     }
+     // check amount is correct length
+     if value.1.len()!= core::mem::size_of::<u64>(){
+        return  Err(ProgramError::InvalidInstructionData);
+     }
+     let lamports:u64 = u64::from_le_bytes(value.1.try_into().unwrap());
+     Ok({Self { owner, vault, lamports }})
+     //cargo add solana-address
+   }
+}  
+
 impl<'info> Deposit<'info>{
-    pub fn process(accounts:&'info[AccountView],instruction_data:&[u8]) -> ProgramResult {
-        let  [owner,vault,_] = accounts else {
-            return Err(pinocchio::error::ProgramError::NotEnoughAccountKeys);
-        };
-       if instruction_data.len()!=core::mem::size_of::<u64>(){
-        return Err(ProgramError::InvalidInstructionData);
-       }
-       let amount:u64 = u64::from_le_bytes(instruction_data.try_into().unwrap());
-       // let amount: u64 = u64::from_le_bytes(instruction_data.try_into().map(|_| ProgramError::InvalidInstructionData));
-        log("Deposit Invoked");
-        Ok(())
+    pub fn process(&self) -> ProgramResult {
+       log("Deposit");
+       Transfer {
+        from:self.owner,
+        to:self.vault,
+        lamports:self.lamports
+       }.invoke();
+       Ok(())
     }
 }
 // cargo  add solana-program-log 
--- first_pinocchio/src/lib.rs
+++ first_pinocchio/src/lib.rs
@@ -1,8 +1,14 @@
 use pinocchio::{AccountView, Address, ProgramResult, entrypoint, error::ProgramError};
+use solana_address::declare_id;
+
+use crate::instructions::Deposit;
 
 entrypoint!(process_instruction);
 
 mod instructions; 
+
+declare_id!("22222222222222222222222222222222222222222222");
+
 fn process_instruction(
     program_id: &Address,      // Address of the account the program was loaded into
     accounts: &[AccountView], // All accounts required to process the instruction
@@ -10,7 +16,7 @@ fn process_instruction(
 ) -> ProgramResult{
     let (discriminator, instruction_data): (&u8, &[u8]) = instruction_data.split_first().ok_or(ProgramError::InvalidInstructionData)?;
     match  *discriminator {
-        0 => Ok(()),
+        0 => Deposit::try_from((accounts,instruction_data))?.process(),
         1 => Ok(()),
         _ => Err(ProgramError::InvalidInstructionData)
     }
